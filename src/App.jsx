6d3a0b7e01429d1e0953fa499005e3d0d5741a56
Wes <piuswesley@gmail.com>
2023-07-13 21:18:01 +0800

    Finish MVP React app as specified

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const url= 'https://api.api-ninjas.com/v1/quotes?category=';
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url, {
+        headers: { 'X-Api-Key': '5HL9hRtaEdLF5JZ1uf1J2w==q76pY7PjSeCRJ836'},
+      });
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {data ? (
+    <div id="quote-box">
+    <div id="text">{data[0].quote}</div>
+    <div id="author">{data[0].author}</div>
+    <div>
+      <button type="submit" onClick={fetchData} id="new-quote">New Quote</button>
+    </div>
+    <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet</a>
+    </div>
+      ) : (
+        <></> // loading data
+      )}
+    </div>
+  );
 }
 
 export default App
+
